61ec18636088d6967df8fb80cb69b9da5b9b06ad
Edward Gutierrez <edward@ext.conviertemas.com>
2020-10-05 22:32:24 -0400

    Adding Docker image

--- RoverNavigationCore/Program.cs
+++ RoverNavigationCore/Program.cs
@@ -21,6 +21,7 @@ namespace RoverNavigationCore
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://*:" + Environment.GetEnvironmentVariable("PORT"));
                 });
     }
 }
